f054fd44f98aa7e7cadc4bcd6776af5ae657c8ea
drgmk <gkennedy@ast.cam.ac.uk>
2016-08-30 09:03:36 +0100

    removed ppmxl dependence, sql password, still testing

--- db-insert-many.sh
+++ db-insert-many.sh
@@ -4,12 +4,21 @@
 
 # some other knobs
 db=sed_db
-user=grant
-pass=grant
+tmp=`cat /etc/my.cnf | grep user | sed 's/ //g'`
+eval $tmp
+tmp=`cat /etc/my.cnf | grep password | sed 's/ //g'`
+eval $tmp
 
 # given sample db name in $1
-mysql -u$user -p$pass $db -N -e "SELECT name FROM $1" | while read name
+mysql -u$user -p$password $db -N -e "SELECT name FROM $1" | while read name
 do
     echo getting:$name
     ./db-insert-one.sh "$name"
 done
+
+# modify source table
+mysql -u$user -p$password $db -N -e "ALTER TABLE $1 ADD COLUMN sdbid BIGINT(19);"
+mysql -u$user -p$password $db -N -e "UPDATE $1 LEFT JOIN xids ON name=xid SET $1.sdbid=xids.sdbid;"
+mysql -u$user -p$password $db -N -e "ALTER TABLE $1 ADD FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);"
+mysql -u$user -p$password $db -N -e "DELETE FROM projects WHERE project='shardds';"
+mysql -u$user -p$password $db -N -e "INSERT INTO projects SELECT sdbid,'$1' FROM $1 WHERE sdbid IS NOT NULL;"
--- sql.sql
+++ sql.sql
@@ -1,37 +1,59 @@
-/* TRIGGERS */
+/* KEYS - run this after recreating all tables with dropcreate, make sure this is done
+   with something found for all tables to get field lengths correct (hd181327). then change
+   to append and run once without exiting for duplicates, then uncomment and go ahead... */
+
+ALTER TABLE sdb_pm ADD PRIMARY KEY (sdbid);
+
+ALTER TABLE xids ADD PRIMARY KEY (sdbid,xid);
+ALTER TABLE xids ADD FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+ALTER TABLE xids ADD UNIQUE (xid);
+ALTER TABLE xids MODIFY xid VARCHAR(100);
+
+CREATE UNIQUE INDEX sdbid_2m ON 2mass (sdbid);
+ALTER TABLE 2mass ADD CONSTRAINT sdbid_2m FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+CREATE UNIQUE INDEX sdbid_ak ON akari_irc (sdbid);
+ALTER TABLE akari_irc ADD CONSTRAINT sdbid_ak FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+CREATE UNIQUE INDEX sdbid_aw ON allwise (sdbid);
+ALTER TABLE allwise ADD CONSTRAINT sdbid_aw FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+CREATE UNIQUE INDEX sdbid_sp ON seip (sdbid);
+ALTER TABLE seip ADD CONSTRAINT sdbid_sp FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+CREATE UNIQUE INDEX sdbid_ty ON tyc2 (sdbid);
+ALTER TABLE tyc2 ADD CONSTRAINT sdbid_ty FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+CREATE UNIQUE INDEX sdbid_sm ON simbad (sdbid);
+ALTER TABLE simbad ADD CONSTRAINT sdbid_sm FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+ALTER TABLE simbad MODIFY main_id VARCHAR(100);
+ALTER TABLE simbad MODIFY sp_type VARCHAR(36);
+ALTER TABLE simbad MODIFY sp_bibcode VARCHAR(19);
+ALTER TABLE simbad MODIFY plx_bibcode VARCHAR(19);
 
 /* remove extra whitespace on names put in xids */
-drop trigger xid_remove_whitespace;
 CREATE TRIGGER xid_remove_whitespace BEFORE INSERT ON `sed_db`.xids FOR EACH ROW SET NEW.xid=trim(replace(replace(replace(replace(replace(NEW.xid,'  ',' '),'  ',' '),'  ',' '),'  ',' '),'  ',' '));
-
 /* same for names in simbad table */
-drop trigger simbad_remove_whitespace;
 CREATE TRIGGER simbad_remove_whitespace BEFORE INSERT ON `sed_db`.simbad FOR EACH ROW SET NEW.main_id=trim(replace(replace(replace(replace(replace(NEW.main_id,'  ',' '),'  ',' '),'  ',' '),'  ',' '),'  ',' '));
+
 
 /* CODE */
 
-/* add samples to projects table */
-alter table sample_shardds add column ppmxl bigint(19);
-update sample_shardds left join xids on name=xid set sample_shardds.ppmxl=xids.ppmxl;
-delete from projects where project='shardds';
-insert into projects select ppmxl,'shardds' from sample_shardds where ppmxl is not null;
-
 /* cleanup to start from scratch */
-truncate table ppmxl;
-truncate table 2mass;
-truncate table akari_irc;
-truncate table allwise;
-truncate table seip;
-truncate table simbad;
-truncate table xids;
-truncate table tyc2;
-truncate table bbfit;
-truncate table stfit;
-truncate table photosphere;
-truncate table totflux;
+DROP TABLE IF EXISTS 2mass;
+DROP TABLE IF EXISTS akari_irc;
+DROP TABLE IF EXISTS allwise;
+DROP TABLE IF EXISTS seip;
+DROP TABLE IF EXISTS simbad;
+DROP TABLE IF EXISTS tyc2;
+DROP TABLE IF EXISTS xids;
+DROP TABLE IF EXISTS sdb_pm;
+
+/* add samples to projects table */
+alter table sample_shardds add column sdbid bigint(19);
+update sample_shardds left join xids on name=xid set sample_shardds.sdbid=xids.sdbid;
+ALTER TABLE sample_shardds ADD FOREIGN KEY (sdbid) REFERENCES sdb_pm (sdbid);
+
+delete from projects where project='shardds';
+insert into projects select sdbid,'shardds' from sample_shardds where sbdid is not null;
 
 /* sed fitting tables */
-CREATE TABLE `totflux` (
+CREATE TABLE `sed_totflux` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) DEFAULT NULL,
   `band` varchar(10) DEFAULT NULL,
@@ -47,7 +69,7 @@ CREATE TABLE `totflux` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1798017 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `bbfit` (
+CREATE TABLE `sed_bbfit` (
   `bbfit_lastupdate` double DEFAULT NULL,
   `name` varchar(30) NOT NULL DEFAULT '',
   `disksig` float DEFAULT NULL,
@@ -87,7 +109,7 @@ CREATE TABLE `bbfit` (
   PRIMARY KEY (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE `photosphere` (
+CREATE TABLE `sed_photosphere` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) DEFAULT NULL,
   `band` varchar(10) DEFAULT NULL,
@@ -103,7 +125,7 @@ CREATE TABLE `photosphere` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1798273 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `stfit` (
+CREATE TABLE `sed_stfit` (
   `stfit_lastupdate` double DEFAULT NULL,
   `name` varchar(30) NOT NULL DEFAULT '',
   `teff` float DEFAULT NULL,
