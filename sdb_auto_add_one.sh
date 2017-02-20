2571db7fbac184a37fd89481f537495787508022
drgmk <gkennedy@ast.cam.ac.uk>
2017-02-20 18:43:33 +0000

    include plot and db options

--- sdb_auto_add_one.sh
+++ sdb_auto_add_one.sh
@@ -86,8 +86,8 @@ popd
 echo "\nRunning sdf"
 pushd $sedroot
 export DYLD_LIBRARY_PATH=/Users/grant/astro/code/github/MultiNest/lib
-python3 /Users/grant/astro/projects/sdf/sdf/fit.py -f $sdbid/public/$sdbid-rawphot.txt --no-spectra
-python3 /Users/grant/astro/projects/sdf/sdf/fit.py -f $sdbid/public/$sdbid-rawphot.txt -u
+python3 /Users/grant/astro/projects/sdf/sdf/fit.py -f $sdbid/public/$sdbid-rawphot.txt --no-spectra -w -b
+python3 /Users/grant/astro/projects/sdf/sdf/fit.py -f $sdbid/public/$sdbid-rawphot.txt -u -w -b
 popd
 
 # remove the lock file
